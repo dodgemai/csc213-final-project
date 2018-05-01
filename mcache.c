979205b34a7c697a6170680c7933f219fe6bc4a3
Maisy Dodge <dodgemai@grinnell.edu>
2018-05-01 09:02:48 -0500

    added map as parameter in mcache.h and implemented mcache.c functions

---
+++ mcache.c
@@ -0,0 +1,25 @@
+#ifndef MCACHE_H
+#define MCACHE_H
+
+#include <stdbool.h>
+#include <entry_list.h>
+
+bool mcache_set(hashmap_t* map, char* key, byte_sequence_t* data){
+  hashmap_put(map, key, data);
+}
+
+bool mcache_add(hashmap_t* map, char* key, byte_sequence_t* data){
+  hashmap_put(map, key, data);
+}
+
+byte_sequence_t* mcache_get(hashmap_t* map, char* key){
+  hashmap_get(map, key);
+}
+
+byte_sequence_t** mcache_gets(hashmap_t* map, char** keys, size_t num_keys){
+  //TODO: add this thing
+}
+
+bool mcache_delete(hashmap_t* map, char* key){
+  hashmap_remove(map, key);
+}
--- mcache.h
+++ mcache.h
@@ -6,14 +6,14 @@
 
 //NOTE TODO might want to separate byte_sequence_t into a more relevant header file!
 
-bool mcache_set(char* key, byte_sequence_t* data);
+bool mcache_set(hashmap_t* map, char* key, byte_sequence_t* data);
 
-bool mcache_add(char* key, byte_sequence_t* data);
+bool mcache_add(hashmap_t* map, char* key, byte_sequence_t* data);
 
-byte_sequence_t* mcache_get(char* key);
+byte_sequence_t* mcache_get(hashmap_t* map, char* key);
 
-byte_sequence_t** mcache_gets(char** keys, size_t num_keys);
+byte_sequence_t** mcache_gets(hashmap_t* map, char** keys, size_t num_keys);
 
-bool mcache_delete(char* key);
+bool mcache_delete(hashmap_t* map, char* key);
 
 #endif
